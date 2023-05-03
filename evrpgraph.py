d2d98a32f57e15d938685fd8525052dfa45c74e9
Vu Quoc Hien <Neih4207@gmail.com>
2023-05-04 02:06:08 +0700

    update plot with path and sol file

--- evrpgraph.py
+++ evrpgraph.py
@@ -1,68 +1,66 @@
-from __future__ import print_function
 import numpy as np
 import matplotlib.pyplot as plt
-np.random.seed(11)
-#!/usr/bin/python
-file_path = 'Data/location/'
-file_name = 'GS_E-n101-k8.evrp_1'
-f = open(file_path + file_name + '.txt', 'r')
-f_location = open(file_path + 'order_' + file_name + '.txt', 'r')
 
-string = f.read()
-str = string.split()
-num_of_node = int(str[0])
+def argparser():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, help='input file', 
+                        default='output_files/1/solution_HMAGS_E-n23-k3.evrp.txt')
+    parser.add_argument('--output', type=str, help='output file', default=None)
+    args = parser.parse_args()
+    return args
 
-string2 = f_location.read()
-str2 = string2.split()
+def readfile(filename):
+    with open(filename, 'r') as f:
+        N = int(f.readline().strip())
+        nodes = []
+        for i in range(N):
+            line = f.readline().strip().split()
+            nodes.append([int(line[0]), float(line[1]), float(line[2])])
+        M = int(f.readline().strip())
+        ordered_nodes = []
+        for i in range(M):
+            line = f.readline().strip().split()
+            ordered_nodes.append([float(line[0]), float(line[1])])
+    return nodes, ordered_nodes
 
-X1 = []
-X2 = []
-
-for a in str2:
-    X2.append(float(a))
-loc = []
-M = len(X2)
-for i in range(M):
-    if(i % 3 == 0):
-        a = []
-        a.append(int(X2[i]))
-        a.append(X2[i + 1])
-        a.append(X2[i + 2])
-        loc.append(a)
+def plotgraph(nodes, ordered_nodes, output = None):
+    """
+    plot the graph
+    """
+    x = []
+    y = []
+    for node in nodes:
+        x.append(node[1])
+        y.append(node[2])
+        type_ = node[0]
+        if type_ == 0:
+            plt.plot(node[1], node[2], 'ro', markersize=5, alpha=.8)
+        elif type_ == 1:
+            plt.plot(node[1], node[2], 'g^', markersize=3, alpha=.8)
+        elif type_ == 2:
+            plt.plot(node[1], node[2], 'bs', markersize=4, alpha=.8)
+        else:
+            raise ValueError('Node type is not correct')
         
-for i in range(1, num_of_node * 2, 2):
-    X1.append([int(str[i]), int(str[i + 1])])
-
-
-def Location(X):
-    for i in range(int(M / 3)):
-        k = X[i][0]
-        x1 = X[i][1]
-        y1 = X[i][2]
-        if k == 1:
-            plt.plot(x1, y1, 'g^', markersize = 3, alpha = .8)
-        if k == 2:
-            plt.plot(x1, y1, 'bs', markersize = 6, alpha = .8)
-    #plt.xlabel('EVRP', color = 'purple')
-    #plt.axis('equal')
-    #plt.plot(X[:, 0], X[:, 1])
-    #plt.show()
-
-def display(X2, save_path='Data/images/', file_name='fig'):
-    for i in range(num_of_node - 1):
-        x1, y1, x2, y2 = X2[i][0], X2[i][1], X2[i + 1][0], X2[i + 1][1]
-        plt.plot([x1, x2], [y1, y2], color = 'tomato', linewidth = 1)
-        #print(x1, y1, x2, y2)
-    #plt.xlabel('EVRP', color = 'purple')
-   #plt.xlabel('Generation', color = 'black')
-    #plt.ylabel('Tour length', color = 'black')
-    #plt.show()
-    plt.plot(loc[0][1], loc[0][2], 'ro', markersize = 7, alpha = .8)
-    plt.savefig(save_path + file_name + '.pdf',bbox_inches='tight')
-    plt.savefig(save_path + file_name + '.png',bbox_inches='tight')
-    #plt.axis('equal')
-    #plt.plot(X[:, 0], X[:, 1])
-    #plt.show()
-
-Location(loc)
-display(X1, file_name=file_name)
+    for i in range(len(ordered_nodes)-1):
+        plt.plot([ordered_nodes[i][0], ordered_nodes[i + 1][0]], 
+                 [ordered_nodes[i][1], ordered_nodes[i+1][1]], color = 'tomato', linewidth = 1)
+    plt.plot([ordered_nodes[-1][0], ordered_nodes[0][0]],
+                [ordered_nodes[-1][1], ordered_nodes[0][1]], color = 'tomato', linewidth = 1)
+    if output is not None:
+        plt.savefig(output)
+    else:
+        plt.show()
+    
+    
+def main():
+    args = argparser()
+    nodes, ordered_nodes = readfile(args.input)
+    output = args.output
+    if output is None:
+        output = ''.join(args.input.split('.')[:-1]) + '.png'
+    plotgraph(nodes, ordered_nodes, output)
+    
+if __name__ == '__main__':
+    main()
